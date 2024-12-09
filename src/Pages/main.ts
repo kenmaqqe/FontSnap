43fe4b6e3c25e05ef187e8484ce0f12e1503016c
Dmytro <dimetriytalibal@gmail.com>
2024-12-09 17:24:34 +0200

    create pages and add navigation

---
+++ src/Pages/main.ts
@@ -0,0 +1,3 @@
+export {default as FontIdentification} from './FontIdentification';
+export {default as TextIdentification} from './TextIdentification';
+export {default as MainPage} from './MainPage';
